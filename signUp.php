a4c2e07d1b7f73f5f8f2709afd3527b7a94f4a0b
ECiccotelli <37254221+ECiccotelli@users.noreply.github.com>
2021-05-10 14:57:29 -0400

    Initial Commit
    Basic PHP functionality to sign up and to log in.

---
+++ signUp.php
@@ -0,0 +1,41 @@
+<DOCTYPE! html>
+
+<html>
+
+<head>
+<link href = "polling.css" type = "text/css" rel = "stylesheet" />
+</head>
+
+<body>
+
+<form action="signup-submit.php" method="post">
+
+<div class = "login">
+
+<fieldset>
+<h1> Create a new account to start making polls! </h1>
+<p>
+<strong>Username: </strong>
+<input type="text" name="username" size="16" required />
+
+<br />
+
+<strong>Password: </strong>
+<input type="password" name="password" size="16" required />
+</p>
+             
+<input class = "button" type="submit" value="Create Account">
+
+</fieldset>
+</form>
+
+</div>
+
+
+
+
+</body>
+
+
+
+</html>
---
+++ signin-submit.php
@@ -0,0 +1,62 @@
+<?php
+// Start the session
+session_start();
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+<link href = "polling.css" type = "text/css" rel = "stylesheet" />
+</head>
+
+
+<body>
+<?php
+
+$username = $_POST["username"];
+$pw = $_POST["password"];
+$user = $username;
+
+
+foreach (file("users.txt", FILE_IGNORE_NEW_LINES) as $storedUsers) {
+	list($prevUsername, $prevPassword) = explode(",", $storedUsers);
+    
+    $username = trim($username);
+    $pw = trim($pw);
+    $prevUsername = trim($prevUsername);
+    $prevPassword = trim($prevPassword);
+    
+    if( (strcmp($username, $prevUsername) == 0) && (strcmp($pw, $prevPassword) == 0)) {
+        echo "Successfully logged in!";
+        $_SESSION["loggedIn"] = "True";
+        $_SESSION["username"] = $username;
+        echo "User is now logged in and loggedIn is set.";
+        print_r($_SESSION["loggedIn"]);
+    }
+}
+    
+?>
+
+
+<div>
+<h1>
+Thank you for signing in! Start creating your polls today!
+</h1>
+<center>
+<form action = "main.php" method = "post" >
+<input class = "button" type = "submit" value = "CLICK HERE TO START SHOPPING">
+</form>
+</center>
+</div>
+
+
+</body>
+
+
+
+
+
+</html>
+
